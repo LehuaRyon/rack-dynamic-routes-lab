254762d1a343bb9a28c2e421156a14c592d0ad97
Jasmine Ryon <jryon12@priorypride.net>
2021-04-20 18:03:33 -0700

    commit tree 08ead7dff3c9a7cb15c182575a5debebb058b103

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+
+    @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            name_of_item = req.path.split("/items/").last
+            if item = @@items.detect{|item| item.name == name_of_item}
+                resp.write item.price
+            else
+                resp.status = 400
+                resp.write "Item not found"
+            end
+        else
+            resp.status = 404
+            resp.write "Route not found"
+        end
+        resp.finish
+    end
+end
